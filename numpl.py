abb6329d69376ab59d1c30ad25e2584e0ecf34c6
PRASUNN <pmprasunn457@gmail.com>
2019-09-25 21:04:43 +0530

    Add files via upload

--- numpl.py
+++ numpl.py
@@ -3,9 +3,15 @@ import json
 from urllib.request import urlopen
 from PIL import Image
 import pytesseract
+import time
 
 #Declare a list to store the values extracted from number plates
 plate=list()
+
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+
+fp=open('solutions.txt', 'w')
+configuration = ("-l eng --oem 1 --psm 11")
 
 #Extract each line from the json file
 for i in open('Indian_Number_plates.json'):
@@ -14,7 +20,7 @@ for i in open('Indian_Number_plates.json'):
     data=json.loads(i)
 
     #Extract each image from the web and convert them into grayscale for better performance
-    img = Image.open(urlopen(data['content'])).convert('LA')
+    img = Image.open(urlopen(data['content']))
 
     #For more than one number plate in one image
     for num in data['annotation']:
@@ -25,7 +31,11 @@ for i in open('Indian_Number_plates.json'):
         #Crop the image to only the numberplate portion
         pic=img.crop(area)
 
+        pic.show()
+
         #Extract the data in the image to string format
-        nmpl=pytesseract.image_to_string(pic)
+        nmpl=pytesseract.image_to_string(pic, config = configuration)
         plate.append(nmpl)
         print(nmpl)
+        fp.write(nmpl)
+        time.sleep(3)
